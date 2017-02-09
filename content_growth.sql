07448472d3616b095f6c5b7e5559e7fe1cead1f8
johnchower <johnchower@gmail.com>
2017-02-09 09:56:55 -0700

    Content growth query.

---
+++ content_growth.sql
@@ -0,0 +1,59 @@
+WITH 
+champion_ids AS (
+SELECT id AS champion_ids
+FROM champion_dimensions
+WHERE id IN (SELECT id FROM champion_dimensions)
+),
+interval_length AS (
+SELECT 30 AS interval_length
+),
+date_ranges AS (
+SELECT id
+, case 
+	when sql_date_stamp + (SELECT interval_length FROM interval_length) 
+		>= current_date
+		AND sql_date_stamp 
+		< current_date
+	THEN 1
+	when sql_date_stamp + 2*(SELECT interval_length FROM interval_length) 
+		>= current_date
+		AND sql_date_stamp + (SELECT interval_length FROM interval_length) 
+		< current_date
+	THEN 0
+	ELSE 2
+end AS date_range
+FROM date_dim
+),
+long_results AS (
+SELECT tcb.champion_id
+	, dr.date_range
+	, count(DISTINCT td.id) AS value
+FROM tree_dimensions td
+left join tree_to_champion_bridges tcb
+ON tcb.tree_id=td.id
+left join date_ranges dr
+ON dr.id=td.created_date_id
+WHERE dr.date_range!=2
+GROUP BY tcb.champion_id, dr.date_range
+),
+wide_results AS (
+SELECT lr.champion_id
+	, sum(lr.date_range*lr.value) AS value_current
+	, sum((1-lr.date_range)*lr.value) AS value_previous
+FROM long_results lr
+GROUP BY lr.champion_id
+)
+SELECT wr.champion_id
+	, cd.NAME AS champion_name
+	, wr.value_previous
+	, wr.value_current
+	, case
+	when wr.value_previous!=0
+		THEN 1.0*(wr.value_current - wr.value_previous)/wr.value_previous 
+	ELSE NULL 
+	END AS pct_change
+FROM wide_results wr
+left join champion_dimensions cd
+ON cd.id=wr.champion_id
+WHERE cd.id IN (SELECT champion_ids FROM champion_ids)
+;
